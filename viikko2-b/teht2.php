3e149d25bbb7ed7fa46d4b8df63467a752fc9a0d
Samu Kangas <samu.kangas@metropolia.fi>
2025-04-01 09:57:45 +0300

    uus repo ku vanhan rakenne meni pieleen

---
+++ viikko2-b/teht2.php
@@ -0,0 +1,41 @@
+<?php
+require_once __DIR__ . '/inc/header.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'):
+    if (isset($_POST['remember'])):
+        setcookie('username', $_POST['username']);
+    else:
+        setcookie('username', '', time() - 3600);
+    endif;
+    header('Location: ' . $_SERVER['PHP_SELF']);
+endif;
+?>
+
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <label>
+            Username
+            <?php
+            $value = '';
+            if (isset($_COOKIE['username'])):
+                $value = "value=$_COOKIE[username]";
+            endif;
+            ?>
+            <input type="text" name="username" placeholder="Username" <?php echo $value; ?>>
+        </label>
+        <label>
+            Remember me
+            <?php
+            $checked = '';
+            if (isset($_COOKIE['username'])):
+                $checked = 'checked';
+            endif;
+            ?>
+            <input type="checkbox" name="remember" <?php echo $checked; ?>>
+        </label>
+        <button type="submit">Submit</button>
+    </form>
+
+
+<?php
+require_once __DIR__ . '/inc/footer.php';
+
